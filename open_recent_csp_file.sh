2238f6502020f9eeabc931c9f27beb7b6b8cdcfb
gokuge <57989544+gokuge@users.noreply.github.com>
2022-04-16 16:02:51 +0900

    Update open_recent_csp_file.sh
    refactoring

--- open_recent_csp_file.sh
+++ open_recent_csp_file.sh
@@ -2,6 +2,6 @@
 
 dir_path="YOUR CLIP STUDIO PAINT FILE DIR PATH"
 array=`find "$dir_path" -name "*.clip" -print0 -o -name "*.cmc" -print0 | xargs -0 ls -t  | head -1`
-if [ ${#array[*]}  -eq 1 ]; then
+if [ ${#array[*]} -eq 1 ]; then
     open "${array[0]}"
 fi
