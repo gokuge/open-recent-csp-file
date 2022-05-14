d7dff78d4c3f9917962244380f6e7b152f4616ab
gokuge <57989544+gokuge@users.noreply.github.com>
2022-05-14 10:20:39 +0900

    Update open_recent_csp_file.sh
    .cmcの検索タイミングを変更

--- open_recent_csp_file.sh
+++ open_recent_csp_file.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 dir_path="YOUR CLIP STUDIO PAINT FILE DIR PATH"
-array=`find "$dir_path" -name "*.clip" -print0 -o -name "*.cmc" -print0 | xargs -0 ls -t  | head -1`
-if [ ${#array[*]} -eq 1 ]; then
-    open "${array[0]}"
+clipArray=`find "$dir_path" -name "*.clip" -print0 | xargs -0 ls -t  | head -1`
+
+# clipArrayが空文字の場合は.clipファイルが存在しないとみなす
+if [ -z "${clipArray[0]}" ]; then
+    return
+fi
+
+# 最新の.clipが取れた。同階層に.cmcがあるか調べる
+dir=$(dirname "${clipArray[0]}")
+cmcArray=`find "$dir" -name "*.cmc" -maxdepth 1 -print0 | xargs -0 ls -t  | head -1`
+
+if [ -n "${cmcArray[0]}" ]; then
+	# .clipが最新だが同階層に.cmcがある。.cmcを開く
+	open "${cmcArray[0]}"
+else
+	# .clipが最新で同階層に.cmcはない。.clipを開く
+	open "${clipArray[0]}"
 fi
